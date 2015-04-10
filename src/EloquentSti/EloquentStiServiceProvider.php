a55d8c7144f920cab5554c689a943f941f057a67
Ryan C. Durham <ryan@stagerightlabs.com>
2015-04-10 18:57:15 +0000

    Clean up code formatting

--- src/EloquentSti/EloquentStiServiceProvider.php
+++ src/EloquentSti/EloquentStiServiceProvider.php
@@ -4,12 +4,12 @@ use Illuminate\Support\ServiceProvider;
 
 class EloquentStiServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
     /**
      * Bootstrap the application events.
@@ -21,24 +21,24 @@ class EloquentStiServiceProvider extends ServiceProvider {
         $this->package('srlabs/eloquent-sti');
     }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
